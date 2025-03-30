a90a32978cbd2871491a36ccd90e2a6c4ad61ca6
AashishAdhikari27 <aashishadhikari2027@gmail.com>
2025-03-30 12:38:59 +0545

    scrapy project created

---
+++ web_scraping/web_scraping/spiders/webspider.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class WebspiderSpider(scrapy.Spider):
+    name = "webspider"
+    allowed_domains = ["www.wedding-spot.com"]
+    start_urls = ["https://www.wedding-spot.com/wedding-venues/?page=1&pr=new%20jersey&r=new%20jersey%3anorth%20jersey&r=new%20jersey%3aatlantic%20city&r=new%20jersey%3ajersey%20shore&r=new%20jersey%3asouth%20jersey&r=new%20jersey%3acentral%20jersey&r=new%20york%3along%20island&r=new%20york%3amanhattan&r=new%20york%3abrooklyn&r=pennsylvania%3aphiladelphia&sr=1"]
+
+    def parse(self, response):
+        pass
